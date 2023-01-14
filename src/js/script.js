c279682e672ab5a2cac6789d1dd0f4db20f8d6fd
Lyutik13 <lyutikov.viktor.96@mail.ru>
2023-01-14 18:07:41 +0300

    +2 saction

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// hamburger
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closse = document.querySelector(".menu__close");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closse.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+// hamburger end
